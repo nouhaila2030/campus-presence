18b4703dd0a061d3b224fba565e48b325f0fe2df
Rania sellak <git>
2026-05-16 10:44:35 +0100

    Premier commit

---
+++ backend/src/main/java/com/example/smartabsence/SmartabsenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartabsence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartabsenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartabsenceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/smartabsence/SmartabsenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartabsence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartabsenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
